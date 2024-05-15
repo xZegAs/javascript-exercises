7483dfd507197c1dd802842dba92c6f44ddfa25a
xZegAs <76397258+xZegAs@users.noreply.github.com>
2024-05-15 07:13:28 +0300

    modified:   07_tempConversion/tempConversion.js 	modified:   07_tempConversion/tempConversion.spec.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(feh) {
+
+  let temp = 0;
+  temp = (feh - 32) * (5 / 9);
+  return Math.round(temp * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let temp = 0;
+  temp = ((9 / 5) * cel) + 32;
+  return Math.round(temp * 10) / 10;
 };
 
 // Do not edit below this line
