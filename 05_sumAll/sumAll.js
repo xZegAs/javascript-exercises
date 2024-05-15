364cb68cd0b1f76fd68fc6f8aa414faa99103162
xZegAs <76397258+xZegAs@users.noreply.github.com>
2024-05-15 06:54:02 +0300

    modified:   05_sumAll/sumAll.js 	modified:   05_sumAll/sumAll.spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    let sum = 0;
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    }
+    else if (num1 < num2){
+        for (let i = num1; i <= num2; i++){
+
+            sum += i;
+    
+        }
+    }
+    else {
+        for (let i = num2; i <= num1; i++){
+
+            sum += i;
+    
+        }
+    }
+    
+
+    return sum;
 
 };
 
