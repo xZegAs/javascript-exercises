544e4b8872a892cd7d18d7427cffa5915641e405
xZegAs <76397258+xZegAs@users.noreply.github.com>
2024-05-16 09:43:22 +0300

    modified:   12_findTheOldest/findTheOldest.js 	modified:   12_findTheOldest/findTheOldest.spec.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,16 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
+
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const personAge = getAge(person.yearOfBirth, person.yearOfDeath);
+        return oldestAge < personAge ? person : oldest;
+    }, people[0]);
 
 };
 
