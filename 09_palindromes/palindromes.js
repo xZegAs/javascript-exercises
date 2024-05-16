aa10d83b9723fd7abe37bb2cf322af80806a550d
xZegAs <76397258+xZegAs@users.noreply.github.com>
2024-05-16 09:15:26 +0300

    modified:   09_palindromes/palindromes.js 	modified:   09_palindromes/palindromes.spec.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const alphanumeric = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanStr = str.toLowerCase().split('').filter(char => alphanumeric.includes(char)).join('');
+    const reversedStr = cleanStr.split('').reverse().join('');
+    return cleanStr === reversedStr;
 };
 
 // Do not edit below this line
