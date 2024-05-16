2586912ba1c004b80e207155662f20ff5b8619ae
xZegAs <76397258+xZegAs@users.noreply.github.com>
2024-05-16 09:07:06 +0300

    modified:   08_calculator/calculator.js 	modified:   08_calculator/calculator.spec.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) return 1;
+  let prod = 1;
+  for (let i = a; i > 0; i--) {
+    prod *= i;
+  }
+  return prod;
 };
 
 // Do not edit below this line
