5aec1b0ee6ec19eb214da1d165bbce33254c731d
xZegAs <76397258+xZegAs@users.noreply.github.com>
2024-05-16 09:27:54 +0300

    modified:   10_fibonacci/fibonacci.js 	modified:   10_fibonacci/fibonacci.spec.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let count;
+    if (typeof num !== 'number') {
+        count = parseInt(num)
+    } else {
+        count = num
+    }
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+    let fib = [0, 1];
+    for (let i = 2; i <= count; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[count];
 };
 
 // Do not edit below this line
